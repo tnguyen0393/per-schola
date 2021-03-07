721b34656d7ede8819af24a3bfce1c3f969bfb5a
Tien Nguyen <tnguyen0393@gmail.com>
2021-03-06 19:40:30 -0600

    outlander obj using inheritance

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6d/605e0545cc7e001b1e60fdb1bc8140b3
@@ -0,0 +1,5 @@
+package Cars;
+
+public class Car {
+
+}
---
+++ Classes Part 1/src/Cars/Main.java
@@ -0,0 +1,11 @@
+package Cars;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Outlander outlander = new Outlander(36);
+		outlander.accelerate(30);
+
+	}
+
+}
